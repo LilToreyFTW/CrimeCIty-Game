3fd0573b9a9a896d68a290c206df7095ebfeae0d
Crime City Developer <developer@crimecity.com>
2025-11-08 02:38:13 -0800

    Fix next.config.js - remove deprecated options

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
-  env: {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  },
   // Enable static file serving
   output: 'standalone',
 }
